aacd012956df0e5b8aa08d924b711d8ea21863cb
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-08-16 20:59:58 +0200

    Register FSR3 present/composition callback to prevent frame flickering

--- Source/FSR3.cpp
+++ Source/FSR3.cpp
@@ -459,8 +459,10 @@ void RTGL1::FSR3::ConfigureFrameGeneration( VkSwapchainKHR swapchain,
     ffxConfigureDescFrameGeneration configDesc = {};
     configDesc.header.type = FFX_API_CONFIGURE_DESC_TYPE_FRAMEGENERATION;
     configDesc.swapChain = (void*)swapchain;
-    configDesc.presentCallback = nullptr;
-    configDesc.presentCallbackUserContext = nullptr;
+    configDesc.presentCallback = []( ffxCallbackDescFrameGenerationPresent* params, void* pUserCtx ) -> ffxReturnCode_t {
+        return ffxDispatch( (ffxContext*)pUserCtx, &params->header );
+    };
+    configDesc.presentCallbackUserContext = &fgContext;
     configDesc.frameGenerationCallback = []( ffxDispatchDescFrameGeneration* params, void* pUserCtx ) -> ffxReturnCode_t {
         return ffxDispatch( (ffxContext*)pUserCtx, &params->header );
     };
