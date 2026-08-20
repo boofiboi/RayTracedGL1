f255ea4c32052d7494e6445afde6811cf2605ec2
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-08-20 11:10:44 +0200

    Configure NRD ReBLUR and ReLAX antilag to eliminate transient light flash ghosting

--- Source/Denoiser.cpp
+++ Source/Denoiser.cpp
@@ -35,6 +35,22 @@ RTGL1::Denoiser::Denoiser( VkInstance _instance,
     , nrdInitialized( false )
 #endif
 {
+#ifdef RG_USE_NVIDIA_NRD
+    reblurSettings.maxAccumulatedFrameNum = 24;
+    reblurSettings.maxFastAccumulatedFrameNum = 4;
+    reblurSettings.diffusePrepassBlurRadius = 0.0f;
+    reblurSettings.specularPrepassBlurRadius = 15.0f;
+    reblurSettings.antilagSettings.luminanceSigmaScale = 1.0f;
+    reblurSettings.antilagSettings.luminanceSensitivity = 1.0f;
+    reblurSettings.enableAntiFirefly = true;
+
+    relaxSettings.diffusePrepassBlurRadius = 0.0f;
+    relaxSettings.specularPrepassBlurRadius = 15.0f;
+    relaxSettings.antilagSettings.accelerationAmount = 0.8f;
+    relaxSettings.antilagSettings.spatialSigmaScale = 2.0f;
+    relaxSettings.antilagSettings.resetAmount = 0.8f;
+#endif
+
     VkDescriptorSetLayout setLayouts[] = {
         framebuffers->GetDescSetLayout(),
         _uniform.GetDescSetLayout(),
